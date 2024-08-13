53b5f64725b1484da07ec7506b7b49bfd83da087
Aakash Acharya <mail.aakash108@gmail.com>
2024-08-13 23:34:02 +1000

    round robin for even number of teams

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let teams = ["team1", "team2", "team3", "team4"]
 
 function roundRobin(teams){
+    const tournament = []
 
-    //creating two halfs of the toal number of teams
-    const half = Math.ceil(teams.length/2)
-    const groupA = teams.slice(0,half)
-    console.log(groupA)
-    const groupB = teams.slice(half,teams.length)
+    let half = Math.ceil(teams.length/2)
+    let groupA = teams.slice(0,half)
+    let groupB = teams.slice(half,teams.length)
     groupB.reverse()
-    console.log(groupB)
-
-
-    //swapping the teams
-
-    for(let i = 1; i<teams.length-1; i++){
-        groupA.splice(1,0, groupB.shift())
-        
-        groupB.push(groupA.pop())
-       
-    }
     console.log(groupA)
-    console.log(groupB)
+    tournament.push(getRound(groupA, groupB))
 
-
-
+    for(let i=1; i<teams.length-1; i++){
+        groupA.splice(1,0,groupB.shift())
+        groupB.push(groupA.pop())
+        tournament.push(getRound(groupA, groupB))
+    }
+    console.log(tournament)
 }
+
+const getRound = (groupA, groupB) => {
+    const total = [];
+    groupA.forEach((p, i) => {
+      total.push([groupA[i], groupB[i]]);
+    });
+    return total;
+  }
 
 roundRobin(teams)
