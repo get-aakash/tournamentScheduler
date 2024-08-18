02d4fb3b343355aa0689fcd0268627e6f8ac7a85
Aakash Acharya <mail.aakash108@gmail.com>
2024-08-18 11:14:06 +1000

    taking input form user

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-let teams = ["team1", "team2", "team3", "team4"]
 
-function roundRobin(teams){
+const generateScheduleEl = document.getElementById("generateSchedule")
+let teamInputEl = document.getElementById("teamInput")
+
+generateScheduleEl.addEventListener("click", function(){
+    const teams = teamInputEl.value.trim().split('\n')
+    roundRobin(teams)   
+})
+const roundRobin = (teams)=>{
     const tournament = []
 
-    let half = Math.ceil(teams.length/2)
-    let groupA = teams.slice(0,half)
-    let groupB = teams.slice(half,teams.length)
+    const half = Math.ceil(teams.length/2)
+    const groupA = teams.slice(0, half)
+    const groupB = teams.slice(half, teams.length)
     groupB.reverse()
-    console.log(groupA)
+
     tournament.push(getRound(groupA, groupB))
 
     for(let i=1; i<teams.length-1; i++){
-        groupA.splice(1,0,groupB.shift())
+        groupA.splice(1,0, groupB.shift())
         groupB.push(groupA.pop())
         tournament.push(getRound(groupA, groupB))
     }
     console.log(tournament)
+    console.log(tournament.length)
+
+    for(let i =0; i<tournament.length;i++){
+        for(let j=0; j<tournament[i].length; j++){
+            console.log(tournament[i][j])
+        }
+    }
+
 }
 
-const getRound = (groupA, groupB) => {
-    const total = [];
-    groupA.forEach((p, i) => {
-      total.push([groupA[i], groupB[i]]);
+const getRound = (groupA, groupB)=>{
+    const total = []
+    groupA.forEach((element, i) => {
+        if (!groupB[i]) {
+            total.push([groupA[i], "bye"]);
+        } else {
+            total.push([groupA[i], groupB[i]]);
+        }
     });
-    return total;
-  }
+    return total
+}
 
-roundRobin(teams)
