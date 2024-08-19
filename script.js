d14a758262f30cd4420134ea4349c4a86ec2ebfd
Aakash Acharya <mail.aakash108@gmail.com>
2024-08-19 21:45:22 +1000

    designing the text area

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 const generateScheduleEl = document.getElementById("generateSchedule")
 let teamInputEl = document.getElementById("teamInput")
+const tableDivEl = document.getElementById("tableDiv")
 
 generateScheduleEl.addEventListener("click", function(){
     const teams = teamInputEl.value.trim().split('\n')
     roundRobin(teams)   
+    teamInputEl.value = ""
 })
 const roundRobin = (teams)=>{
     const tournament = []
@@ -21,14 +23,10 @@ const roundRobin = (teams)=>{
         groupB.push(groupA.pop())
         tournament.push(getRound(groupA, groupB))
     }
-    console.log(tournament)
-    console.log(tournament.length)
+   
+    generateTable(tournament)
 
-    for(let i =0; i<tournament.length;i++){
-        for(let j=0; j<tournament[i].length; j++){
-            console.log(tournament[i][j])
-        }
-    }
+  
 
 }
 
@@ -44,3 +42,30 @@ const getRound = (groupA, groupB)=>{
     return total
 }
 
+const generateTable = (tournament) => {
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    for (let i = 0; i < tournament.length; i++) {
+        const roundRow = document.createElement("tr");
+        const roundHeaderCell = document.createElement("td");
+        roundHeaderCell.colSpan = tournament[i].length;
+        roundHeaderCell.style.fontWeight = "bold";
+        roundHeaderCell.style.textAlign = "center";
+        roundHeaderCell.appendChild(document.createTextNode("Round " + (i+1)));
+        roundRow.appendChild(roundHeaderCell);
+        tblBody.appendChild(roundRow);
+
+        for (let j = 0; j < tournament[i].length; j++) {
+            const row = document.createElement("tr");
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(`${tournament[i][j][0]} vs ${tournament[i][j][1]}`);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+            tblBody.appendChild(row);
+        }
+    }
+
+    tbl.appendChild(tblBody);
+    tableDivEl.appendChild(tbl);
+};
